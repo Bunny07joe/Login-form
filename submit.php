36235ff01e95c168453cc126953b65f9740c4a49
VEGAD MITESH M <165547209+Bunny07joe@users.noreply.github.com>
2024-04-10 01:39:38 -0400

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,5 +1,5 @@
 <?php
-// Step 3: Connect to MySQL database
+// Step 1: Connect to MySQL database
 $servername = "localhost"; // Change this if your MySQL server is on a different host
 $username = "root"; // Change this to your MySQL username
 $passwords = "Meet@222"; // Change this to your MySQL password
@@ -13,12 +13,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Step 4: Sanitize and validate user input (for simplicity, only basic sanitization is demonstrated)
+// Step 2: Sanitize and validate user input (for simplicity, only basic sanitization is demonstrated)
 $names = mysqli_real_escape_string($conn, $_POST['name']); // Escape special characters to prevent SQL injection
 $passwords = mysqli_real_escape_string($conn, $_POST['password']);
 // You can add more fields as per your form
 
-// Step 5: Insert data into MySQL database
+// Step 3: Insert data into MySQL database
 $sql = "INSERT INTO users (names, password) VALUES ('$names', '$passwords')";
 
 if ($conn->query($sql) === TRUE) {
