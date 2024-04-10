f657f5b735a3c41e59dc13d679c8c80eb4abcf52
VEGAD MITESH M <165547209+Bunny07joe@users.noreply.github.com>
2024-04-10 11:08:19 +0530

    Add files via upload

---
+++ show.php
@@ -0,0 +1,31 @@
+<?php
+// Step 3: Connect to MySQL database
+$servername = "localhost"; // Change this if your MySQL server is on a different host
+$username = "root"; // Change this to your MySQL username
+$passwords = "Meet@222"; // Change this to your MySQL password
+$dbname = "Student"; // Change this to your MySQL database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $passwords, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Step 5: Show data from MySQL database
+$sql = "SELECT * FROM users";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo " Name: " . $row["names"]. " - Password: " . $row["password"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
